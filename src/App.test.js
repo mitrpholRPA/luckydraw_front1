4e1b1e1e3e11bd6de8c0b9349d1b3ece2df61210
mitrpholRPA <145524964+mitrpholRPA@users.noreply.github.com>
2024-11-18 14:48:19 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react';  // นำเข้า act จาก react
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);  // ใช้ act ล้อมรอบการเรนเดอร์
+  });
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
