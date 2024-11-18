aeb5b020b4fcce4659890d1eb23e6ffd32c69def
mitrpholRPA <rparobot@mitrphol.com>
2024-11-18 23:00:41 +0700

    production

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 // setupTests.js
 global.matchMedia = global.matchMedia || function() {
     return {
